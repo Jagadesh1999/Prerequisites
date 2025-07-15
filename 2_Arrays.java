39159ed3d5488cb8a1cce214768bef8042a40766
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-07-15 22:01:04 +0530

    Update 2_Arrays.java

--- 2_Arrays.java
+++ 2_Arrays.java
@@ -1,1 +1,26 @@
+import java.io.*;
+import java.util.*;
 
+// 1. Max element in the array
+public class Main {
+    public static void solve() {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+        for(int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int max = arr[0];
+        for(int i = 0; i < size; i++) {
+            if(arr[i] > max) max = arr[i];
+        }
+
+        System.out.println(max);
+    }
+
+    public static void main(String[] args) {
+        solve();
+    }
+}
