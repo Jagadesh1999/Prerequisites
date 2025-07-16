0a1b2271e753105df2a22bc57ecbe127ef9b4273
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-07-16 21:07:01 +0530

    Update 2_Arrays.java

--- 2_Arrays.java
+++ 2_Arrays.java
@@ -18,7 +18,7 @@ public class Main {
         }
 
         System.out.println(sol);
-    }
+    } // TC : O(n)
 
     public static void main(String[] args) {
         solve();
@@ -45,7 +45,7 @@ public class Main {
         }
 
        for(int i = 0; i < size; i++) System.out.print(arr[i] + " "); 
-    }
+    } // TC : O(n)
 
     public static void main(String[] args) {
         solve();
@@ -68,7 +68,63 @@ public class Main {
         }
 
         System.out.print(oddSum + " " + evenSum);
+    } // TC : O(n)
+
+    public static void main(String[] args) {
+        solve();
     }
+}
+
+// 4. Count the occurences of zeroes, positives and negatives in the array.
+public class Main {
+    public static void solve() {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+
+        int[] nums = new int[size];
+        for(int i = 0; i < size; i++) nums[i] = sc.nextInt();
+
+        int countPositive, countNegative, countZeroes;
+        countPositive = countNegative = countZeroes = 0;
+
+        for(int i = 0; i < size; i++) {
+            if(nums[i]==0) countZeroes ++;
+            else if(nums[i]>0) countPositive ++;
+            else if(nums[i]<0) countNegative ++;
+        }
+
+        System.out.print(countZeroes + " " + countPositive + " " + countNegative);
+    } // TC : O(n)
+
+    public static void main(String[] args) {
+        solve();
+    }
+} 
+
+// 5. Mean, median and mode
+
+// 6. The Missing Number
+public class Main {
+    public static void solve() {
+        Scanner sc = new Scanner(System.in);
+
+        int[] nums = new int[99];
+        for(int i = 0; i < 99; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int[] freq_nums = new int[101];
+        for(int i = 0; i < 99; i++) {
+            freq_nums[nums[i]]++;
+        }
+
+        for(int i = 1; i <= 100; i++) {
+            if(freq_nums[i] == 0) {
+                System.out.print(i);
+                return;
+            }
+        }
+    } // TC : O(n)
 
     public static void main(String[] args) {
         solve();
