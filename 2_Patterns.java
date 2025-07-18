c3e032484780f52cab0f358c61d83d201e87a2d6
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-07-18 20:27:38 +0530

    Update 2_Patterns.java

--- 2_Patterns.java
+++ 2_Patterns.java
@@ -1,1 +1,25 @@
+import java.io.*;
+import java.util.*;
 
+// 1. Floyd Pattern - 1
+public class Main {
+    public static void solve() {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        int count = 0;
+        for(int i = 0; i < num; i++) {
+            for(int j = 0; j < num; j++) {
+                if(i >= j) {
+                    count ++;
+                    System.out.print(count + " ");
+                }
+            }
+            System.out.println();
+        }
+    } // TC : O(n * n)
+
+    public static void main(String[] args) {
+        solve();
+    }
+}
