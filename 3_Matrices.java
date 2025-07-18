5acc8ae72c02c29c9e20694525df577e25084ec7
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-07-18 11:50:34 +0530

    Update 3_Matrices.java

--- 3_Matrices.java
+++ 3_Matrices.java
@@ -1,1 +1,28 @@
+// 1. Print Matrix sum row - wise
+public class Main {
+    public static void solve() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[][] mat = new int[n][m];
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            int sum = 0;
+            for(int j = 0; j < m; j++) {
+                sum += mat[i][j];
+            }
+            System.out.println(sum);
+        }
+    } // TC : O(n * m)
+
+    public static void main(String[] args) {
+        solve();
+    }
+}
 
