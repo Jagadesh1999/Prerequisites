e9c4d77e42c9adedfc823789ee3d23eb63c317d4
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-07-18 22:03:10 +0530

    Update 2_Patterns.java

--- 2_Patterns.java
+++ 2_Patterns.java
@@ -1,7 +1,26 @@
 import java.io.*;
 import java.util.*;
 
-// 1. Floyd Pattern - 1
+// 1. Triangle Pattern
+public class Main {
+    public static void solve() {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for(int i = 0; i < num; i++) {
+            for(int j = 0; j < num; j++) {
+                if(i >= j) System.out.print("* ");
+            }
+            System.out.println();
+        }
+    } // TC : O(n * n)
+
+    public static void main(String[] args) {
+        solve();
+    }
+} 
+
+// 2. Floyd Pattern - 1
 public class Main {
     public static void solve() {
         Scanner sc = new Scanner(System.in);
@@ -23,3 +42,30 @@ public class Main {
         solve();
     }
 }
+
+// 3. Rectangle Pattern
+public class Main {
+    public static void solve() {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for(int i = 0; i < num; i++) {
+            int temp = num;
+            for(int j = 0; j < num; j++) {
+                if(i + j == num - 1) {
+                    System.out.print("*");
+                    temp --; 
+                }
+                else {
+                   System.out.print(temp);
+                   temp --; 
+                }
+            }
+            System.out.println();
+        }
+    } // TC : O(n * n)
+
+    public static void main(String[] args) {
+        solve();
+    }
+} 
