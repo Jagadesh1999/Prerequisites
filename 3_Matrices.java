63a2493f0e025cf29db2cf66d680a09a524a9963
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-07-18 11:55:12 +0530

    Update 3_Matrices.java

--- 3_Matrices.java
+++ 3_Matrices.java
@@ -1,4 +1,4 @@
-// 1. Print Matrix sum row - wise
+// 1. Print matrix sum row - wise
 public class Main {
     public static void solve() {
         Scanner sc = new Scanner(System.in);
@@ -26,3 +26,30 @@ public class Main {
     }
 }
 
+// 2. Print matrix sum column wise
+public class Main {
+    public static void solve() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[][] mat = new int[n][m];
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int j = 0; j < m; j++) {
+            int sum = 0;
+            for(int i = 0; i < n; i++) {
+                sum += mat[i][j];
+            }
+            System.out.println(sum);
+        }
+    } // TC : O(m * n)
+
+    public static void main(String[] args) {
+        solve();
+    }
+}
