8a8152e77cb2afedf75c74ed3e6e343de36add68
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-07-20 11:51:45 +0530

    Update 1_Conditionals and Loops.cpp

--- 1_Conditionals and Loops.java
+++ 1_Conditionals and Loops.java
@@ -1,134 +1,81 @@
-import java.io.*;
-import java.util.*;
+#include <bits/stc++.h>
+using namespace.std;
 
 // 1. Triangle Validator
-public class Main {
-    public static void solve() {
-        int num_1, num_2, num_3;
-
-        Scanner sc = new Scanner(System.in);
-        num_1 = sc.nextInt();
-        num_2 = sc.nextInt();
-        num_3 = sc.nextInt();
-
-        if((num_1 + num_2 > num_3) && (num_2 + num_3 > num_1) &&(num_1 + num_3 > num_2)) {
-            System.out.println("Yes");
-        } else System.out.println("No");
-    }
-
-    public static void main(String[] args) {
-        solve();
-    }
+void solve1() {
+    int num_1, num_2, num_3;
+    cin >> num_1 >> num_2 >> num_3;
+    if((num_1 + num_2 > num_3) && (num_2 + num_3 > num_1) && (num_1 + num_3 > num_2)) {
+        cout << "Yes" << endl;
+    } else cout << "No" << endl;
 }
 
 // 2. Area of the rectangle
-public class Main {
-    public static void solve() {
-        Scanner sc = new Scanner(System.in);
-        long length = sc.nextLong();
-        long breadth = sc.nextLong();
-
-        long area = length * breadth;
-
-        System.out.println(area);
-    }
-
-    public static void main(String[] args) {
-        solve();
-    }
+void solve2() {
+    long long length, breadth;
+    cin >> length >> breadth;
+    long long area = length * breadth;
+    cout << area << endl;
 }
 
 // 3. Factorial Problem
-public class Main {
-    public static void factorial() {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-
-        if(num == 0) {
-            System.out.println("1");
-            return;
-        }
-
-        int fact = 1;
-        for(int i = 1; i <= num; i++) {
-            fact = fact * i;
-        }
-
-        System.out.println(fact);
+void factorial() {
+    int num;
+    cin >> num;
+    if(num == 0) {
+        cout << "1" << endl;
+        return;
     }
-
-    public static void main(String[] args) {
-        factorial();
+    int fact = 1;
+    for(int i = 1; i <= num; i++) {
+        fact = fact * i;
     }
+    cout << fact << endl;
 }
 
 // 4. Number of multiples of 3 and 5 between 1 and n
-public class Main {
-    public static void solve() {
-        Scanner sc = new Scanner(System.in);
-        long num = sc.nextLong();
-
-        // Inclusion Exclusion Principle
-        long count_3 = num / 3;
-        long count_5 = num / 5;
-        long count_15 = num / 15; // LCM of (3, 5)  = 15
-
-        System.out.println(count_3 + count_5 - count_15);
-    }
-
-    public static void main(String[] args) {
-        solve();
-    }
+void solve4() {
+    long long num;
+    cin >> num;
+    // Inclusion Exclusion Principle
+    long long count_3 = num / 3;
+    long long count_5 = num / 5;
+    long long count_15 = num / 15; // LCM of (3, 5)  = 15
+    cout << count_3 + count_5 - count_15 << endl;
 }
 
 // 5. Sum of multiples of 3 from 1 to n
-public class Main
-{
-    public static void solve() {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        
-        int sum = 0;
-        for(int i = 0; i <= num; i++) {
-            if(i % 3 == 0) {
-                sum = sum + i;
-            }
+void solve5() {
+    int num;
+    cin >> num;
+    
+    int sum = 0;
+    for(int i = 0; i <= num; i++) {
+        if(i % 3 == 0) {
+            sum = sum + i;
         }
-        
-        System.out.println(sum);
     }
     
-	public static void main(String[] args) {
-		solve();
-	}
+    cout << sum << endl;
 }
 
 // 6. Nth Fibonacci number
-public class Main {
-    public static void solve() {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-
-        if(num < 2) {
-            System.out.print(num);
-            return;
-        }
-
-        int a = 0; 
-        int b = 1;
-        int sum = 0;
-
-        for(int i = 2; i <= num; i++) {
-            sum = a + b;
-            a = b;
-            b = sum;
-        }
-        System.out.print(sum);
+void solve6() {
+    int num;
+    cin >> num;
+    if(num < 2) {
+        cout << num;
+        return;
     }
-
-    public static void main(String[] args) {
-        solve();
+    int a = 0; 
+    int b = 1;
+    int sum = 0;
+    for(int i = 2; i <= num; i++) {
+        sum = a + b;
+        a = b;
+        b = sum;
     }
+    cout << sum;
 }
 
 // 7. Armstrong number
@@ -136,40 +83,46 @@ public class Main {
 // -- Count the total number of digits.
 // -- For each digit, find the digit ^ count and add it to the sum value.
 // -- Check if the final sum is equal to the input number.
-public class Main {
-    public static void solve() {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        
-        // int count = 0;
-        int temp_1, temp_2; temp_1 = temp_2 = num;
-        
-        // while(temp_1 != 0) {
-        //     temp_1 /= 10;
-        //     count ++;
+void solve7() {
+    int num;
+    cin >> num;
+    
+    // int count = 0;
+    int temp_1, temp_2; temp_1 = temp_2 = num;
+    
+    // while(temp_1 != 0) {
+    //     temp_1 /= 10;
+    //     count ++;
+    // }
+    int count = to_string(num).length();
+    
+    int sum = 0;
+    while(num != 0) {
+        int digit = num % 10;
+        // int prod = 1;
+        // for(int i = 1; i <= count; i++) {
+        //     prod = prod * digit;
         // }
-        int count = String.valueOf(num).length();
-        
-        int sum = 0;
-        while(num != 0) {
-            int digit = num % 10;
-            // int prod = 1;
-            // for(int i = 1; i <= count; i++) {
-            //     prod = prod * digit;
-            // }
-            int prod = (int) Math.pow(digit, count);
-            sum = sum + prod;
-            num /= 10;
-        }
-       
-       if(sum == temp_2) {
-           System.out.println("Yes");
-           return;
-       }
-       System.out.println("No");
-    } // TC : O(digits * digits)
-
-    public static void main(String[] args) {
-	   solve();
+        int prod = (int) pow(digit, count);
+        sum = sum + prod;
+        num /= 10;
     }
+   
+   if(sum == temp_2) {
+       cout << "Yes" << endl;
+       return;
+   }
+   cout << "No" << endl;
+} // TC : O(digits * digits)
+
+int main() {
+    // Uncomment the function you want to test
+    // solve1();
+    // solve2();
+    // factorial();
+    // solve4();
+    // solve5();
+    // solve6();
+    solve7();
+    return 0;
 }
