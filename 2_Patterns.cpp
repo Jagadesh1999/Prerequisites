e646f04cde34f5816be2007fdac9b8dae316a1b1
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-07-20 11:58:03 +0530

    Update and rename 2_Patterns.java to 2_Patterns.cpp

---
+++ 2_Patterns.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+// 1. Triangle Pattern
+void solve() {
+    int num;
+    cin >> num;
+    for(int i = 0; i < num; i++) {
+        for(int j = 0; j < num; j++) {
+            if(i >= j) cout << "* ";
+        }
+        cout << endl;
+    }
+} // TC : O(n * n)
+
+int main() {
+    solve();
+    return 0;
+}
+
+/*
+// 2. Floyd Pattern - 1
+void solve() {
+    int num;
+    cin >> num;
+    int count = 0;
+    for(int i = 0; i < num; i++) {
+        for(int j = 0; j < num; j++) {
+            if(i >= j) {
+                count ++;
+                cout << count << " ";
+            }
+        }
+        cout << endl;
+    }
+} // TC : O(n * n)
+
+int main() {
+    solve();
+    return 0;
+}
+*/
+
+// 3. Rectangle Pattern
+void solve() {
+    int num;
+    cin >> num;
+    for(int i = 0; i < num; i++) {
+        int temp = num;
+        for(int j = 0; j < num; j++) {
+            if(i + j == num - 1) {
+                cout << "*";
+                temp --; 
+            }
+            else {
+               cout << temp;
+               temp --; 
+            }
+        }
+        cout << endl;
+    }
+} // TC : O(n * n)
+
+int main() {
+    solve();
+    return 0;
+}
+*/
