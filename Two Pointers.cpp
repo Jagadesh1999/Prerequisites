1dd989104c2d27b1d1de75777486c27c9b1c91b8
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-08-04 23:24:09 +0530

    Update Two Pointers.cpp

--- Two Pointers.cpp
+++ Two Pointers.cpp
@@ -1,7 +1,10 @@
-Problem Statement: Identify the longest subarray composed exclusively of 1's in a binary array, while allowing for the option to flip at most k 0's to 1's.
+// Problem Statement: Identify the longest subarray composed exclusively of 1's in a binary array, while allowing for 
+// the option to flip at most k 0's to 1's.
 
-To address this problem, we will devise a general template and explore its application for solving similar problems of this nature.
+// To address this problem, we will devise a general template and explore its application for solving similar problems of 
+// this nature.
 
+// Template : 
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -45,6 +48,57 @@ int main()
         else
         {
             // change data  structure because removing tail element
+            
+            tail++;
+        }
+    }
+
+    cout << ans << endl;
+}
+
+// Code for the above problem using the template 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    // head and tail
+    int tail = 0, head = -1;
+
+    // datastructure for the window
+	int cntzero = 0;
+	
+    // answer maintain
+    int ans = 0;
+
+    while (tail < n)
+    {
+        while (head+1 < n && (arr[head+1]==1 || cntzero<k)
+        {
+            head++;
+            if(arr[head]==0)cntzero++;
+        }
+       
+        
+        ans = max(ans, head-tail+1);
+        
+        // move start one step forward.
+        if (tail > head)    // keep it same for all question, it basically help to handle empty subarray
+        {
+            tail++;
+            head = tail - 1;
+        }
+        else
+        {
+            if(arr[tail]==0)cntzero--;
+
             
             tail++;
         }
